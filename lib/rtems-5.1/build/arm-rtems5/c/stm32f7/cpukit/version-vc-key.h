6b5cefdfeddda6225291956b740bc0d643f640cb
Karthikeyan Vijayakumar <karthikeyanvijayakumar@hotmail.com>
2020-11-27 06:17:07 -0800

    Remove the now ignored directory build

--- lib/rtems-5.1/build/arm-rtems5/c/stm32f7/cpukit/version-vc-key.h
+++
@@ -1,7 +0,0 @@
-/*
- * Automatically generated. Do not edit.
- */
-#if !defined(_RTEMS_VERSION_VC_KEY_H_)
-#define _RTEMS_VERSION_VC_KEY_H_
-#define RTEMS_VERSION_VC_KEY "13bcd87116d5752f22576f5fe49032151edd62f8-modified"
-#endif
